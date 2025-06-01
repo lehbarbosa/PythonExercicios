2d9dcdf2964c96e644c7ba2b255ec9e034e1ce74
Leandro Barbosa <lehbarbosa87@gmail.com>
2025-06-01 16:11:23 -0300

    changed ex010

--- ex010.py
+++ ex010.py
@@ -1,4 +1,4 @@
-dinheiro = float(input('Dgite quanto de dinheiro tem em carteira: '))
+dinheiro = float(input('Digite quanto de dinheiro tem em carteira: '))
 
 dolar = dinheiro * 3.27
 print('Como esse dinheiro você pode compra US${} dólares.\n'.format(dolar))
