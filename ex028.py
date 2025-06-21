3011311b80bb1e6bc7f48d411e66fb66b8b9893e
Leandro Barbosa <lehbarbosa87@gmail.com>
2025-06-21 16:08:59 -0300

    resolvido ex028 e 029

--- ex028.py
+++ ex028.py
@@ -1,5 +1,18 @@
-# Escreva um programa que faça o computador "pensear" em um 
+# Escreva um programa que faça o computador "pensar" em um 
 # número inteiro entre 0 e 5 e peça para o usuário tentar 
 # descobrir qual foi o número escolhido pelo computador.
 # O programa deverá escrever na tela se o usuário venceu ou
 # perdeu.
+import random
+import time
+
+num_pc = random.randint(0, 5)
+print('Vou pensar num número entre 0 e 5 tente adivinhar...')
+time.sleep(1)
+num_usuario = int(input('Em que número estou pensando? '))
+print('Processando...')
+time.sleep(1)
+if num_usuario == num_pc:
+    print(f"PARABÉNS! Você venceu pensei no número {num_pc}")
+else:
+    print(f"GANHEI! Eu pensei no número {num_pc} e não no {num_usuario}!")
--- ex029.py
+++ ex029.py
@@ -1,3 +1,12 @@
 # Escreva um programa que leia a velocidade de um carro.
 # Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado.
 # A multa vai custar R$7,00 por cada km acima do limite.
+velocidade_carro = int(input('Digite a velocidade do carro em km/h: '))
+
+if velocidade_carro > 80:
+    print('Você foi multado!')
+    multa = (velocidade_carro - 80) * 7
+    print(f'O valor da sua multa vai custar R${multa:.2f}')
+else:
+    print('PARABÉNS, você está andando dentro do limite de velocidade, continue assim.')
+
