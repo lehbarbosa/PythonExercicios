cf50491528d9a57ac421f9a4ce6831b00f670bf1
Leandro Barbosa <lehbarbosa87@gmail.com>
2025-06-23 12:03:19 -0300

    teste de commit via command line

--- ex031.py
+++ ex031.py
@@ -1,3 +1,12 @@
-# Desenvolva um programa que pergunte a distância de um viagem em km.
+# Desenvolva um programa que pergunte a distância de uma viagem em km.
 # Calcule o preço da passagem, cobrando R$0,50 por km para viagem até 200km
 # e R$0,45 para viagens mais longas.
+distancia = float(input('Digite a distância percorrida na viagem (km): '))
+
+if distancia <= 200:
+    preco_viagem = distancia * 0.50
+    print(f'Sua passagem vai custar R${preco_viagem:.2f}')
+else:
+    preco_viagem = distancia * 0.45
+    print(f'Sua passagem vai custar R${preco_viagem:.2f}')
+print('Tenha uma boa viagem!')
