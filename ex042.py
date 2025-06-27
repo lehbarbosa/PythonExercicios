ab0022bc94e62e648f2945bd66b2af32732f889d
Leandro Barbosa <lehbarbosa87@gmail.com>
2025-06-27 12:47:29 -0300

    27-06-2025

--- ex042.py
+++ ex042.py
@@ -8,4 +8,18 @@ será formado:
 - Escaleno: todos os lados diferentes
 """
 
+print('-=' * 20)
+print('Analisador de Triângulo')
+print('-=' * 20)
 
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceito segment: '))
+
+if r1 == r2 and r2 == r3 and r1 == r3:
+    print('Com os segmentos digitados forma-se um triângulo: EQUILÁTERO!')
+elif r1 != r2 and r2 != r3 and r1 != r3:
+    print('Com os segmentos digitados forma-se um triângulo: ESCALENO!')
+else:
+    print('Com os segmentos digitados forma-se um triângulo: iSÓSCELES !')
+    
