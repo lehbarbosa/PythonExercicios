2754a555e1e644001829aba4257a6396e39ba674
Leandro Barbosa <lehbarbosa87@gmail.com>
2025-06-27 12:09:54 -0300

    27-06-2025

--- ex041.py
+++ ex041.py
@@ -9,6 +9,63 @@ e mostre sua categoria, de acordo com a idade:
 - Até 20 anos: Sênior
 - Acima: Master
 """
+from datetime import date
+
+ano_de_nascimeto = int(input('Informe o ano de nascimento do atleta: '))
+ano_atual = date.today().year
+
+idade = ano_atual - ano_de_nascimeto
+
+if idade <= 9:
+    print(f'O atleta tem idade de {idade} anos.')
+    print('A categoria do atleta é: Mirim')
+elif idade <= 14:
+    print(f'O atleta tem idade de {idade} anos.')
+    print('A categoria do atleta é: Infantil')
+elif idade <= 19:
+    print(f'O atleta tem idade de {idade} anos.')
+    print('A categoria do atleta é: Junior')
+elif idade <= 20:
+    print(f'O atleta tem idade de {idade} anos.')
+    print('A categoria do atleta é: Sênior')
+else:
+    print(f'O atleta tem idade de {idade} anos.')
+    print('A categoria do atleta é: Master')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
