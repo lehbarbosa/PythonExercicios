c2777f12d54baba42aeacb2fb1f250d839c90a28
Leandro Barbosa <lehbarbosa87@gmail.com>
2025-07-01 11:12:45 -0300

    resolvendo ex com for

--- ex047.py
+++ ex047.py
@@ -2,4 +2,18 @@
 Crie um programa que mostrer na tela todos os números pares que estão 
 no intervalo entre 1 e 50.
 """
+print('*' * 40)
+print(f'{"Contagem de Números Pares (1 a 50)":^40}')
+print('*' * 40)
 
+print('Forma de resolução 1:')
+for pares in range(1, 51):
+    if pares % 2 == 0:
+        par = pares
+        print(f'{par}', end=' ')
+
+# Forma de resolução 2:
+print('\n')
+print('Forma de resolução 2:')
+for par in range(2, 51, 2):
+    print(f'{par}', end=' ')
--- ex048.py
+++ ex048.py
@@ -2,3 +2,11 @@
 Faça um programa que calcule a soma entre todos os números impares 
 que são múltiplos de três e que se encontram no intervalo de 1 até 500.
 """
+print('*' * 50)
+print(f'{"Contagem de Números Ímpares (1 a 500)":^50}')
+print('*' * 50)
+
+for impares in range(1, 501):
+    if (impares % 2 == 1) and (impares / 3 == 0):
+        impar = impares
+        print(f'{impar}', end=' ')
