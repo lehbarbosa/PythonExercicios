1a909233885c11abe7b0530f9e62b435e55c8c17
Leh Barbosa <lehbarbosa87@gmail.com>
2025-07-07 10:29:44 -0300

    .

--- ex053.py
+++ ex053.py
@@ -7,3 +7,16 @@ A TORRE DA DERROTA
 O LOBO AMA O BOLO
 ANOTARAM A DATA DA MARATONA
 """
+frase = str(input('Digite uma frase: ')).strip().upper()
+frase_separada = frase.split()
+junto = ''.join(frase_separada)
+frase_invertida = ''
+
+for palavra in range(len(junto) - 1, -1, -1):
+    frase_invertida += junto[palavra]
+    
+print(F'O inverso de {junto} é {frase_invertida}')
+if frase_invertida == junto:
+    print('Temos um palíndromo!')
+else:
+    print('Frase NÃO É UM PALÍNDROMO!')
--- ex054.py
+++ ex054.py
@@ -1,6 +1,6 @@
 """
 Crie um programa que leia o ano de nascimento de sete pessoas. 
 No final, mostre quantas pessoas ainda não atingiram a maioridade e
- quantas já são maiores.
+quantas já são maiores.
 """
 
