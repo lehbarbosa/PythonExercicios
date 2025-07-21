a67450e55fc24b60a5cf88521b8b29f759f5172e
Leh Barbosa <lehbarbosa87@gmail.com>
2025-07-20 21:14:24 -0300

    .

--- ex055.py
+++ ex055.py
@@ -1,19 +1,26 @@
 """
 Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi MAIOR e o MENOR peso lidos.
 """
+
+print('\033[33m=\033[m' * 30)
+print(f'\033[32m{"Lendo peso de 5 pessoas":^30}\033[m')
+print('\033[33m=\033[m' * 30)
+
 peso_maior = 0
 peso_menor = 0
 for pess in range(1, 6):
     peso = float(input(f'Digite o peso da {pess}ª: '))
-    # Verificando maior peso
-    peso_maior = peso
-    if peso_maior >= peso and peso_maior < peso:
+
+    if pess == 1:
         peso_maior = peso
-    # Verificando menor peso
-    if peso_menor < peso_maior:
         peso_menor = peso
+    else:
+    # Verificando menor peso
+        if peso > peso_maior:
+            peso_maior = peso
+    # Verificando menor peso
+        if peso < peso_menor:
+            peso_menor = peso
 
-print(f'O peso MAIOR foi de {peso_maior}')
-print(f'O peso MENOR foi de {peso_menor}')
-
-
+print(f'\033[33mO peso MAIOR foi de {peso_maior}kg\033[m')
+print(f'\033[32mO peso MENOR foi de {peso_menor}kg\033[m')
--- ex056.py
+++ ex056.py
@@ -1,7 +1,33 @@
-"""Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. 
+"""
+Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. 
 No final do programa, mostre: 
 EX.:
 > A média de idade do grupo.
 > Qual é o nome do homem mais velho.
 > Quantas mulheres têm menos de 20 anos.
 """
+soma_idade = 0
+idade_maior = 0
+nome_mais_velho = ''
+cont_mulher_menor_20 = 0
+for pes in range(1, 5):
+    nome = str(input(f'Digite o nome {pes}ª pessoa: '))
+    idade = int(input(f'Digite a idade da {pes}ª pessoa: '))
+    sexo = str(input(f'Digite o sexo da {pes}ª pessoa: ')).upper()
+    # Calculo da média:
+    soma_idade += idade
+    media = soma_idade / pes
+    # Calculo idade Homem mais velho:
+    if sexo == 'M':
+        if idade > idade_maior:
+            idade_maior = idade
+            nome_mais_velho = nome
+    # Calculo idade Mulher menor de 20 anos:
+    if sexo == 'F':
+        if idade < 20:
+            cont_mulher_menor_20 += 1
+print()
+print('=' * 30)
+print(f'A média de idade do grupo é de \033[32m{media}\033[m')
+print(f'O nome do homem mais velho \033[33m{nome_mais_velho}\033[m')
+print(f'Temos \033[35m{cont_mulher_menor_20}\033[m mulher menores de 20 anos.')
