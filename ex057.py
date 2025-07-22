438d3f5e2673125956d5c0693e882119bd60cede
Leandro Barbosa <lehbarbosa87@gmail.com>
2025-07-21 21:24:36 -0300

    .

---
+++ ex057.py
@@ -0,0 +1,10 @@
+'''
+Exercício Python 57: 
+Faça um programa que leia o sexo de uma pessoa, mas só
+aceite os valores 'M' e 'F'.
+Caso esteja errado, peça a digitação novamente  até ter um
+valor correto.
+'''
+
+
+
