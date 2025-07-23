7fa7f768dfd6a7eb9ad022e282f1cf355be0bc6f
Leh Barbosa <lehbarbosa87@gmail.com>
2025-07-22 21:15:13 -0300

    ex 058
     incluído uma pequena interatividade com usuário

--- ex058.py
+++ ex058.py
@@ -7,13 +7,15 @@ palpites foram necessários para vencer
 '''
 from random import randint
 num_pc = randint(0, 10)
+print('\033[33mSou seu computador... Acabei de pensar em um número entre 0 e 10.\033[m')
+print('\033[33mSerá que você consegue adivinhar qual foi?\033[m')
 num_jogador = 0
 cont_tentativas = 0
 while num_jogador != num_pc:
-    num_jogador = int(input('Em que número estou pensando? '))
-    print('Processando...')
-    print('Tente novamente!')
+    num_jogador = int(input('Qual seu palpite? '))
     cont_tentativas += 1
-
-print('\033[32mPARABÉNS! \nVocê acertou o número que pensei foi {}\033[m'.format(num_pc))
-print('\033[33mVocê preciso de {} tentativas\033[m'.format(cont_tentativas))
+    if num_jogador > num_pc:
+        print('Menos... Tente mais uma vez.')
+    elif num_jogador < num_pc:
+        print('Mais... Tente mais uma vez.')
+print('\033[32mAcertou com {} tentativas. PARABÉNS!\033[m'.format(cont_tentativas))
