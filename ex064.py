c7277aa6be6e1de601158c466d8c02beb987b418
Leandro Barbosa <lehbarbosa87@gmail.com>
2025-07-25 17:40:22 -0300

    .

--- ex064.py
+++ ex064.py
@@ -5,6 +5,17 @@ O programa só vai parar quando o usuário digitar o valor 999,
 que é a condição de parada. No final, mostre quantos números 
 foram digitados e qual foi o soma entre eles (desconsiderando o flag).
 '''
-
-
+print('Para cancelar digite 999')
+num = 0
+totnum = 0
+soma = 0
+while num != 999:
+    num = int(input('Digite um número inteiro '))
+    if num == 999:
+        num = 999
+    else:
+        totnum += 1
+        soma += num
+print('Você digito um total de {} número'.format(totnum))
+print('A soma de todos o valores digitado é {}'.format(soma))
 
