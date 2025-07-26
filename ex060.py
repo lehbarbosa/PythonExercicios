ef25e0a3f3b871b225e70cae87204e952a3d9c65
Leh Barbosa <lehbarbosa87@gmail.com>
2025-07-25 21:16:06 -0300

    .

--- ex060.py
+++ ex060.py
@@ -7,9 +7,22 @@ Ex.:
 5!=5x4x3x2x1=120
 '''
 num = int(input('Digite um número para ver seu fatorial: '))
-cont = 1
-fatorial = num
-while cont < num:
-    fatorial *= num - cont
-    cont += 1
-print('O fatorial de {}! é {}'.format(num, fatorial))
+cont = num
+fatorial = 1
+
+print('Calculando {}! = '.format(num), end=' ')
+while cont > 0:
+    print('{}'.format(cont), end=' ')
+    print(' x ' if cont > 1 else ' = ', end=' ' )
+    fatorial *= cont 
+    cont -= 1
+print('{}'.format(fatorial))
+
+
+# print('Calculando {}!'.format(num), end=' ')
+# for cont in range(num, 0, -1):
+#     print('{}'.format(cont), end=' ')
+#     print(' x ' if cont > 1 else ' = ', end=' ')
+#     fatorial *= cont
+# print('{}'.format(fatorial))
+
--- ex065.py
+++ ex065.py
@@ -5,6 +5,25 @@ No final da execução, mostre a média entre todos os valores
 e qual foi o maior e o menor valores lidos. O programa
 deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 '''
-
+sair = 'S'
+media = 0
+totvalor = 0
+cont = 0
+maior = None
+menor = None
+while sair != 'N':
+    num = int(input('Digite um número inteiro: '))
+    sair = str(input('Deseja continuar [S/N]: ')).strip().upper()
+    
+    totvalor += num
+    cont += 1
+    if maior is None or num > maior:
+        maior = num
+    if menor is None or num < menor:
+        menor = num
+media = totvalor / cont
+print('A média dos valores digitados é {:.2f}'. format(media))
+print('O MAIOR valor digitado é {}'. format(maior))
+print('O MENOR valor digitado é {}'. format(menor))
 
 
