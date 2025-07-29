a56350c274e67e4824a4fb60ee4e80ec61a6bdbd
Leh Barbosa <lehbarbosa87@gmail.com>
2025-07-29 19:30:55 -0300

    .

--- ex062.py
+++ ex062.py
@@ -4,4 +4,12 @@ Melhore o DESAFIO 061, perguntando para o usuário se ele
 quer mostrar mais alguns termos. O programa encerra quando 
 ele disser que quer mostrar 0 termos.
 '''
-
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} -> '.format(termo), end=' ')
+    termo += razao
+    cont += 1
+print('FIM')
