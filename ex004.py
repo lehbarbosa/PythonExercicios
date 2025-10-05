44e4adb4292ce7ed9d6fd5f3c0995d47c0bf739b
Leandro Barbosa <leandrobarbosa1705@gmail.com>
2025-10-05 13:46:09 -0300

    Ajustado alguns metodos do ex004

--- ex004.py
+++ ex004.py
@@ -3,8 +3,11 @@ Faça um programa que leia algo pelo teclado e mostre
 na tela o seu tipo primitivo e todas as informações possiveis sobre ele.
 '''
 n = input('Digite algo: ')
-print(type(n))
-print('É numerico: {}'.format(n.isalnum()))
-print('Tem caractere alfabetico: {}'.format(n.isalpha()))
-print('Convete caractere minuscula: {}'.format(n.islower()))
-print('Convete caractere maiuscula: {}'.format(n.isupper()))
+print('O tipo primitivo desse valor é ', type(n))
+print('Só tem espaços? ', n.isspace())
+print('É um número: ',n.isnumeric())
+print('É alfabetico: ',n.isalpha())
+print('É alfanumerico: ', n.isalnum())
+print('É minuscula: ', n.islower())
+print('É maiuscula: ',n.isupper())
+print('Esta capitalizada: ', n.istitle())
