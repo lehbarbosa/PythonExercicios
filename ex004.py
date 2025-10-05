73483fd054ae84ddfd017d3b4ae419724a2e0bce
Leandro Barbosa <leandrobarbosa1705@gmail.com>
2025-10-04 23:19:40 -0300

    .

--- ex004.py
+++ ex004.py
@@ -2,3 +2,9 @@
 Faça um programa que leia algo pelo teclado e mostre
 na tela o seu tipo primitivo e todas as informações possiveis sobre ele.
 '''
+n = input('Digite algo: ')
+print(type(n))
+print('É numerico: {}'.format(n.isalnum()))
+print('Tem caractere alfabetico: {}'.format(n.isalpha()))
+print('Convete caractere minuscula: {}'.format(n.islower()))
+print('Convete caractere maiuscula: {}'.format(n.isupper()))
