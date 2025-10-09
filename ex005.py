d125ecce1ab9f2804612abc1bd3aebfbf76e7c65
Leandro Barbosa <leandrobarbosa1705@gmail.com>
2025-10-09 20:56:05 -0300

    09 exercicíos  de Python 3 - Mundo 1 - CursoemVideo

---
+++ ex005.py
@@ -0,0 +1,4 @@
+'''
+Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e
+seu antecessor.
+'''
---
+++ ex010.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa que leia quanto de dinheiro um pessoa tem na carteira e mostre
+quantos dólares ela pode comprar.
+
+Considere
+US$1.00 = R$3.27
+'''
