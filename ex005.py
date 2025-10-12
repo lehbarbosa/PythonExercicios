f5fbb10a5875eda0399d7f57bccbd8403f2d50af
Leandro Barbosa <leandrobarbosa1705@gmail.com>
2025-10-12 13:19:39 -0300

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -2,3 +2,5 @@
 Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e
 seu antecessor.
 '''
+num = int(input('Digite um valor: '))
+print('O sucessor de {} é {}'.format(num, num+1))
