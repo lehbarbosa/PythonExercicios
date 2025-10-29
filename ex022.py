9fd3ac0e8303580489f23ca32a8d9a260e1f84f8
Leandro Barbosa <leandrobarbosa1705@gmail.com>
2025-10-28 21:11:53 -0300

    resolvido ex022

--- ex022.py
+++ ex022.py
@@ -5,3 +5,8 @@ Crie um programa que leia o nome completo de uma pessoa e mostre:
 > Quantas letras ao todo (sem considerar espaços);
 > Quantas letras tem o primeiro nome.
 """
+nome = str(input("Digite seu nome completo: ")).strip()
+print("Seu nome com letras maiúsculas é: {}".format(nome.upper()))
+print("Seu nome com letras minusculas é: {}".format(nome.lower()))
+print("Seu nome tem {} letras ao todo.".format(len(nome.replace(' ',''))))
+print("Seu primeiro nome é {} tem {} letras".format(nome.split()[0], len(nome.split()[0])))
