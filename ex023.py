c9f7f00e53cb0db0e2720790a83e796f895e8c54
Leandro Barbosa <leandrobarbosa1705@gmail.com>
2025-10-29 20:36:49 -0300

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -1,5 +1,5 @@
 """
-Faça um programa que leia um número de 0 a 999 e mostre na tela cada um dos 
+Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos 
 dígitos separados.
 
 Ex: 
@@ -10,3 +10,17 @@ dezena: 3
 centena: 8
 milhar: 1
 """
+num = int(input("Digite um número de 0 a 9999: "))
+unidade = num // 1 % 10
+dezena = num // 10 % 10 
+centena = num // 100 % 10
+milhar = num // 1000 % 10
+
+print("""
+Analisando o número {}:
+
+Unidade: {}
+Dezena: {}
+Centena: {}
+Milhar: {}
+""".format(num, unidade, dezena, centena, milhar))
