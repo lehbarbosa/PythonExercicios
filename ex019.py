c29236612e9bd5b935da127b8335337eb9f289d4
Leh Barbosa <leandrobarbosa1705@gmail.com>
2026-01-06 13:51:31 -0300

    resolvendo ex

--- ex019.py
+++ ex019.py
@@ -3,3 +3,12 @@ Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o
 nome do escolhido.
 """
+import random
+
+aluno1 = input('Digite o nome do primeiro aluno: ')
+aluno2 = input('Digite o nome do segundo aluno: ')
+aluno3 = input('Digite o nome do terceiro aluno: ')
+aluno4 = input('Digite o nome do quarto aluno: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+ordem = random.choice(lista)
+print('O aluno sorteado foi >>> {}'.format(ordem))
--- ex020.py
+++ ex020.py
@@ -3,3 +3,13 @@ O mesmo professor do desafio 019 quer sortear a ordem de apresentação de
 trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e
 mostre a ordem sorteada.
 """
+from random import shuffle
+
+aluno1 = input('Primeiro nome: ')
+aluno2 = input('Segundo nome: ')
+aluno3 = input('Terceiro nome: ')
+aluno4 = input('Quarto nome: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+shuffle(lista)
+print('A ordem das apresentações será')
+print(lista)
