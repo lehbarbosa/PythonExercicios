da64042912c83fbbe6531cc57ed6b0915d88e50e
Leh Barbosa <leandrobarbosa1705@gmail.com>
2026-01-07 14:58:53 -0300

    Resolvendo

--- ex022.py
+++ ex022.py
@@ -6,3 +6,12 @@ Crie um programa que leia o nome completo de uma pessoa e mostre:
 > Quantas letras ao todo (sem considerar espaços).
 > Quantas letras tem o primeiro nome.
 """
+
+nome = input('Digite seu nome completo: ')
+nome_sem_espaços = nome.replace(' ', '')
+print('Analisando seu nome...')
+print('Seu nome em letras maiúsculas: {}'.format(nome.upper()))
+print('Seu nome em letras minúsculas: {}'.format(nome.lower()))
+print('Seu nome completo tem {} letras sem considerar espaços.'.format(len(nome_sem_espaços)))
+nome_separado = nome.split()
+print('Seu primeiro nome tem {} letras.'.format(len(nome_separado[0])))
--- ex024.py
+++ ex024.py
@@ -2,3 +2,6 @@
 Crie um programa que leia o nome de uma cidade e diga se ela
 começa ou não com o nome 'SANTO'.
 """
+nome = input('Digite o nome de uma cidade: ').upper()
+
+print('A cidade que você digito tem "SANTO "')
